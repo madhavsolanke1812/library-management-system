8d05b4844bd72d6972d06379de96bdc1dbf0c034
Madhav <patilms471@gmail.com>
2026-01-14 00:08:00 +0530

    Initial commit

--- src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -2,10 +2,11 @@ package com.booktrack.librarymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableCaching
 @EnableScheduling
 public class LibraryManagementSystemApplication {
 
