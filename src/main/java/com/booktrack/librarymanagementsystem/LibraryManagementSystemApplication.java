57b1900018e85de2e2c2840d9232bb3e20fa34c1
Madhav <patilms471@gmail.com>
2026-01-14 20:20:02 +0530

    Cache Disabled

--- src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -2,11 +2,10 @@ package com.booktrack.librarymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
+//@EnableCaching
 @EnableScheduling
 public class LibraryManagementSystemApplication {
 
