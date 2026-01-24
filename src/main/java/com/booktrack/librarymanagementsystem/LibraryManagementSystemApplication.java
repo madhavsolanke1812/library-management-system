4ed3faff9826de499a491e8128911d0221275ac1
Madhav <patilms471@gmail.com>
2026-01-25 02:36:59 +0530

    Change for ECS

--- src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/booktrack/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -11,8 +11,8 @@ public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementSystemApplication.class, args);
-        System.out.println("Library Book Management System Application.");
+        System.out.println("Library Book Management System Application-Demo Check");
+
+
     }
-
-
 }
