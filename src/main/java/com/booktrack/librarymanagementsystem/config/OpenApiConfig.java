00d53b7d21cea33d259735538c0b60c2361f659f
Madhav <patilms471@gmail.com>
2026-01-25 18:48:53 +0530

    CodePepiline

--- src/main/java/com/booktrack/librarymanagementsystem/config/OpenApiConfig.java
+++ src/main/java/com/booktrack/librarymanagementsystem/config/OpenApiConfig.java
@@ -1,16 +1,13 @@
 package com.booktrack.librarymanagementsystem.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI libraryOpenApi() {
-        return new OpenAPI().info(new Info().title("Library Book Management API").version("1.0")
-                .description("API for Library Book Management System"));
-    }
+//    @Bean
+//    public OpenAPI libraryOpenApi() {
+//        return new OpenAPI().info(new Info().title("Library Book Management API").version("1.0")
+//                .description("API for Library Book Management System"));
+//    }
 }
